269fbd1553bb8c77023142aa8362a38fcf94d1bb
mikhail-belii <sasama15555@gmail.com>
2025-04-24 21:38:45 +0700

    [UserService] UserController доделан(добавлена документация эндпоинтов)

--- User Service/PresentationLayer/Controllers/UserController.cs
+++ User Service/PresentationLayer/Controllers/UserController.cs
@@ -14,12 +14,30 @@ namespace User_Service.PresentationLayer.Controllers;
 public class UserController : ControllerBase
 {
     private readonly IUserService _userService;
+    
+    /// <summary>
+    /// Get applicant's profile
+    /// </summary>
+    /// <param name="page">Page number</param>
+    /// <response code="200">Education programs list was received</response>
+    /// <response code="500">InternalServerError</response>
 
     public UserController(IUserService userService)
     {
         _userService = userService;
     }
 
+    /// <summary>
+    /// Get applicant's profile
+    /// </summary>
+    /// <response code="200">Applicant's profile was received</response>
+    /// <response code="403">Forbidden</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">InternalServerError</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApplicantModel))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseModel))]
     [Authorize]
     [HttpGet("applicant/profile/{targetUserId:guid}")]
     public async Task<ActionResult<ApplicantModel>> GetApplicantsProfile([FromRoute] Guid targetUserId)
@@ -29,6 +47,17 @@ public class UserController : ControllerBase
         return Ok(response);
     }
     
+    /// <summary>
+    /// Get manager's profile
+    /// </summary>
+    /// <response code="200">Manager's profile was received</response>
+    /// <response code="403">Forbidden</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">InternalServerError</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ManagerModel))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseModel))]
     [Authorize]
     [HttpGet("manager/profile/{targetUserId:guid}")]
     public async Task<ActionResult<ManagerModel>> GetManagersProfile([FromRoute] Guid targetUserId)
@@ -38,6 +67,19 @@ public class UserController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Edit applicant's profile
+    /// </summary>
+    /// <response code="200">Applicant's profile was edited</response>
+    /// <response code="400">Bad request</response>
+    /// <response code="403">Forbidden</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">InternalServerError</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApplicantModel))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseModel))]
     [Authorize]
     [HttpPut("applicant/edit-profile/{targetUserId:guid}")]
     public async Task<ActionResult<ApplicantModel>> EditApplicantsProfile(
@@ -64,6 +106,19 @@ public class UserController : ControllerBase
         return Ok(response);
     }
     
+    /// <summary>
+    /// Edit applicant's password (for applicant only)
+    /// </summary>
+    /// <response code="200">Applicant's password was edited</response>
+    /// <response code="400">Bad request</response>
+    /// <response code="403">Forbidden</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">InternalServerError</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseModel))]
     [Authorize]
     [HttpPut("applicant/edit-password")]
     public async Task<ActionResult<ResponseModel>> ApplicantEditApplicantsPassword(
@@ -89,6 +144,19 @@ public class UserController : ControllerBase
         return Ok(response);
     }
     
+    /// <summary>
+    /// Edit applicant's password (for manager only)
+    /// </summary>
+    /// <response code="200">Applicant's password was edited</response>
+    /// <response code="400">Bad request</response>
+    /// <response code="403">Forbidden</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">InternalServerError</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseModel))]
     [Authorize]
     [HttpPut("manager/edit-applicants-password")]
     public async Task<ActionResult<ResponseModel>> ManagerEditApplicantsPassword(
@@ -114,6 +182,19 @@ public class UserController : ControllerBase
         return Ok(response);
     }
     
+    /// <summary>
+    /// Edit manager's credentials
+    /// </summary>
+    /// <response code="200">Manager's credentials were edited</response>
+    /// <response code="400">Bad request</response>
+    /// <response code="403">Forbidden</response>
+    /// <response code="404">Not found</response>
+    /// <response code="500">InternalServerError</response>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseModel))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseModel))]
     [Authorize]
     [HttpPut("manager/edit-credentials")]
     public async Task<ActionResult<ResponseModel>> EditManagersCredentials([FromBody] ManagerEditCredentialsModel managerEditCredentialsModel)
